1406ecc691e3eb63cb2e09587187234ce40bd027
dymchenkko <elizavetadymchenko20@gmail.com>
2022-06-11 19:41:12 +0200

    6/11/2022

--- methods/guest/src/bin/json_work.rs
+++ methods/guest/src/bin/json_work.rs
@@ -1,13 +1,22 @@
 #![no_main]
 #![no_std]
+#[cfg(target_has_atomic)]
+
 use risc0_zkvm_guest::{env, sha};
 use checker_core::MyStruct;
+use  signature_bls::{Signature, PublicKey};
+
 risc0_zkvm_guest::entry!(main);
 #[macro_use]
 extern crate alloc;
 pub fn main() {
-    let my_struct: MyStruct = env::read(); 
+   /* let my_struct: MyStruct = env::read(); 
     let my_struct_str = format!("{} ", my_struct);
-    let my_struct_str = my_struct_str.as_bytes();    
-    env::commit(&sha::digest_u8_slice(my_struct_str));
+    let my_struct_str = my_struct_str.as_bytes();*/
+    let pk: PublicKey = env::read();
+    let sign: Signature = env::read();
+    let msg: &str = env::read();
+    let result = sign.verify(pk, msg);
+    //env::commit();  
+   // env::commit(&sha::digest_u8_slice(my_struct_str));
 }
--- starter/src/main.rs
+++ starter/src/main.rs
@@ -1,17 +1,38 @@
 use std::fs;
+use sha2::{Sha256, Digest};
 use methods::{JSON_WORK_ID, JSON_WORK_PATH};
 use risc0_zkvm_host::Prover;
 use risc0_zkvm_serde::{from_slice, to_vec};
 use tempfile::tempdir;
-use risc0_zkvm_core::Digest;
 use checker_core::MyStruct;
 #[macro_use]
 extern crate alloc;
 use std::time::Instant;
+use signature_bls::SecretKey;
+use  signature_bls::Signature;
+use  signature_bls::PublicKey;
+use rand::{rngs::OsRng, RngCore};
 fn main() {
-    let a: MyStruct = MyStruct{
+
+    let mut hasher = Sha256::new();
+    let data = b"Hello worldjiojoijojihygtft t!";
+    hasher.update(data);
+    // `update` can be called repeatedly and is generic over `AsRef<[u8]>`
+    hasher.update("String data");
+    // Note that calling `finalize()` consumes hasher
+    let mut rand_generator = OsRng {};
+
+    let hash = hasher.finalize();
+    rand_generator.next_u32();
+
+    let sk = SecretKey::random(rand_generator).unwrap();
+    let signat = Signature::new(&sk, &"data").unwrap();
+    let pk = PublicKey::from(&sk);
+    let nn = signat.verify(pk, "data");
+    println!("Binary hash: {:?}", hash);    let a: MyStruct = MyStruct{
         arr: 125,
     };
+    println!("Binary hash: {:?}", nn);
     let temp_dir = tempdir().unwrap();
     let id_path = temp_dir
         .path()
@@ -21,16 +42,19 @@ fn main() {
         .to_string();
     fs::write(&id_path, JSON_WORK_ID).unwrap();
     let mut prover = Prover::new(&JSON_WORK_PATH, &id_path).unwrap();
-    prover.add_input(to_vec(&a).unwrap().as_slice()).unwrap();
+    prover.add_input(to_vec(&pk).unwrap().as_slice()).unwrap();
+    prover.add_input(to_vec(&signat).unwrap().as_slice()).unwrap();
+    prover.add_input(to_vec(&"data").unwrap().as_slice()).unwrap();
 
     let now = Instant::now();
     let receipt = prover.run().unwrap();
     let elapsed = now.elapsed();
 
     let c = receipt.get_seal().unwrap();
-    let c: Result<Digest, _> = from_slice(c);
+   /* let c: Result<risc0_zkvm_core::Digest, _> = from_slice(c);
     let c = c.unwrap();
-    let c: String = format!("{:?}", c);
+    let mut result = c.as_slice();
+    let c: String = format!("{:?}", c);*/
     println!("Elapsed: {:.3?}", elapsed);
-    println!("I know the factors of {:?}, and I can prove it!", c);
+    //println!("I know the factors of {:?}, and I can prove it!", c);
 }
